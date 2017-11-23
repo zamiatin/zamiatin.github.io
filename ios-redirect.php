983c54b16c389a93ec5b340c371f61cce3f76fc6
zyama <zyama@syneforge.com>
2017-11-23 18:00:50 +0200

    Update php file

--- ios-redirect.php
+++ ios-redirect.php
@@ -1,3 +1,9 @@
 <?php
     header('X-Frame-Options: GOFORIT');
+
+    fucntion redirect() {
+      'com.apple.mobilesafari://' + 'x-callback-url/' + 'openTab?x-source=' + 'http://letitstars.com'
+    }
+
+    redirect();
 ?>
