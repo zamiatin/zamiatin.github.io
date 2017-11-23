1670df818e0bdb5880ee36c124eb9f654ded5cd7
zyama <zyama@syneforge.com>
2017-11-23 11:52:53 +0200

    Update

--- ios-redirect.php
+++ ios-redirect.php
@@ -1,3 +1,29 @@
 <?php
     header('X-Frame-Options: GOFORIT');
 ?>
+
+NSURL *inputURL = <the URL to open>;
+NSString *scheme = inputURL.scheme;
+
+// Replace the URL Scheme with the Chrome equivalent.
+NSString *chromeScheme = nil;
+if ([scheme isEqualToString:@"http"]) {
+  chromeScheme = @"googlechrome";
+} else if ([scheme isEqualToString:@"https"]) {
+  chromeScheme = @"googlechromes";
+}
+
+// Proceed only if a valid Google Chrome URI Scheme is available.
+if (chromeScheme) {
+  NSString *absoluteString = [inputURL absoluteString];
+  NSRange rangeForScheme = [absoluteString rangeOfString:@":"];
+  NSString *urlNoScheme =
+      [absoluteString substringFromIndex:rangeForScheme.location];
+  NSString *chromeURLString =
+      [chromeScheme stringByAppendingString:urlNoScheme];
+  NSURL *chromeURL = [NSURL URLWithString:chromeURLString];
+
+  // Open the URL with Chrome.
+  [[UIApplication sharedApplication] openURL:chromeURL];
+}
+
