056ca8315418d551c5e7b6436f62c4da35ce733c
zyama <zyama@syneforge.com>
2018-01-26 11:52:59 +0200

    Landing upload

--- ios-redirect.php
+++
@@ -1,8 +0,0 @@
-<?php
-  // Редирект из нативных мобильных приложений в Safari
-  header('Location: mobile-app-client-cv://x-callback-url/');
-  exit;
-?>
-
-
-<!-- com.apple.mobilesafari://x-callback-url/openTab?x-source= -->
