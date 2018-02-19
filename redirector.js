6811daaaffb2ce648b87d3956977b28f6d454a95
zyama <zyama@syneforge.com>
2018-02-19 14:39:41 +0200

    Update

--- redirector.js
+++ redirector.js
@@ -122,7 +122,7 @@ if (/instagram/i.test(navigator.userAgent)) {
                     instaRedirect.setLocation('googlechrome://' + clearUrl);
                     setTimeout(function(){
                         desktopRedirect();
-                    }, 100);
+                    }, 200);
                 } else {
                     instaRedirect.setLocation(protocol + '://' + clearUrl);
                 }
